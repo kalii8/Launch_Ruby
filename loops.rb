7d7773aea64d05b432aec04a3bdf075c40b1d3b1
kalii8 <liva.karlivane@gmail.com>
2018-10-14 15:51:06 +0000

    loops exercises

---
+++ loops.rb
@@ -0,0 +1,30 @@
+#2
+# x = ""
+# while x != "STOP" do
+#   puts ("Hello! Write something")
+#   answer = gets.chomp
+#   puts ("STOP?")
+#   x = gets.chomp
+
+# end
+
+#3
+
+# array = ["cat", "dog", "elephant"]
+
+# array.each_with_index do |item, index| 
+#   puts "#{index + 1}. #{item}"
+# end
+
+#4
+
+# def countdown(number)
+#   if number <= 0
+#     puts number
+#   else
+#     puts number
+#     countdown(number-1)
+#   end
+# end
+
+# countdown(-4)
